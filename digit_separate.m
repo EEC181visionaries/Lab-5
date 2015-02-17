ba901f861e96ae3e39e155ce24ddb3274e10e820
ChanPhilip77 <Zahl506@gmail.com>
2015-02-16 16:05:47 -0800

    Update digit_separate.m

--- digit_separate.m
+++ digit_separate.m
@@ -38,6 +38,57 @@ for j= 1:4:col
             if ( bad ~= 1)
                 digit = roi(1:row,last_mid:mid);
                 digit = imresize(digit,[28,28]);
+                
+                
+                
+                %center image vertically
+                digit_left = 1;
+                digit_right = 28;
+                for k = 1:28
+                    if (any(digit(:,k)))
+                        digit_left = k;
+                        break;
+                    end
+                end
+                for k = 28:-1:1
+                   if (any(digit(:,k)))
+                       digit_right = k;
+                       break;
+                   end
+                end
+                digit_width = digit_right - digit_left;
+                left_add = idivide((27-digit_width),int8(2),'ceil');
+                right_add = idivide((27-digit_width),int8(2),'floor');
+                left_add = zeros(28,left_add);
+                right_add = zeros(28,right_add);
+                digit = [left_add digit(:,digit_left:digit_right) right_add];
+                %
+                
+                % center image horizontally
+                digit_top = 1;
+                digit_bottom = 28;
+                for k = 1:28
+                    if (any(digit(k,:)))
+                        digit_top = k;
+                        break;
+                    end
+                end
+                for k = 28:-1:1
+                   if (any(digit(k,:)))
+                       digit_bottom = k;
+                       break;
+                   end
+                end
+                digit_height = digit_bottom - digit_top;
+                top_add = idivide((27-digit_height),int8(2),'ceil');
+                bottom_add = idivide((27-digit_height),int8(2),'floor');
+                top_add = zeros(top_add,28);
+                bottom_add = zeros(bottom_add,28);
+                digit = [top_add;digit(digit_top:digit_bottom,:);bottom_add];
+                
+                
+                
+                
                 digit_vector = reshape(digit,784,1);
                 digit_matrix = [digit_matrix, digit_vector];
                 last_mid = mid;
